c0684ace317597bfedf352072801237051fe7852
MadeInOuweland <loek@6wunderkinder.com>
2014-08-04 23:31:32 +0200

    * initial

---
+++ EscapeTheEnter/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace EscapeTheEnter
+{
+    sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame {Language = Windows.Globalization.ApplicationLanguages.Languages[0]};
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+            Window.Current.Activate();
+        }
+
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ EscapeTheEnter/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using Windows.Foundation;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+
+namespace EscapeTheEnter
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private readonly List<IDisposable> _subscriptions = new List<IDisposable>();
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var keys = Observable.FromEventPattern<TypedEventHandler<CoreWindow, KeyEventArgs>, KeyEventArgs>(
+                h => Window.Current.CoreWindow.KeyDown += h, h => Window.Current.CoreWindow.KeyDown -= h)
+                .Select(pattern => pattern.EventArgs);
+
+            _subscriptions.Add(keys.Where(x => x.VirtualKey == VirtualKey.Enter).Subscribe(_ => Accept()));
+            _subscriptions.Add(keys.Where(x => x.VirtualKey == VirtualKey.Escape).Subscribe(_ => Cancel()));
+        }
+
+        private void Accept()
+        {
+            KeyBlock.Text = "Enter -> Accept";
+        }
+
+        private void Cancel()
+        {
+            KeyBlock.Text = "Escape -> Cancel";
+        }
+        
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            foreach (var sub in _subscriptions)
+            {
+                sub.Dispose();
+            }
+            _subscriptions.Clear();
+        }
+    }
+}
